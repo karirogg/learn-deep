a5bcf929bd049c558153a247f618e16f7cfe9bd3
Paul Buttler <paul.buttler10@gmail.com>
2024-12-16 19:46:18 +0100

    add varation ratio

--- src/metrics/mc_dropout.py
+++ src/metrics/mc_dropout.py
@@ -1,19 +1,16 @@
 import torch
+import pandas as pd
 
 
 def mc_dropout_inference(
-    model, dataloader, device, num_samples=50, classification=True
+    model, dataloader, device, num_samples=100, classification=True
 ):
     """
-    Perform MC Dropout inference over a dataloader to compute mean and variance of predictions.
+    Perform MC Dropout inference over a dataloader to compute mean, variances and uncertainty measures of predictions.
     """
 
     model.train()   # Enable dropout during inference
-    mean_predictions = []
-    variances = []
-
-    if classification:
-        expected_entropy = []
+    all_predictions = []
 
     with torch.no_grad():
         for inputs, _ in dataloader:
@@ -21,47 +18,46 @@ def mc_dropout_inference(
             batch_predictions = []
 
             for _ in range(num_samples):
+                outputs = model(inputs)  # [batch_size, num_classes/output_dim]
                 if classification:
-                    outputs = model(inputs)     # [batch_size, num_classes]
                     outputs = torch.nn.functional.softmax(outputs, dim=1)
-                else:
-                    outputs = model(inputs)
-
                 batch_predictions.append(outputs)
 
-            batch_predictions = torch.stack(batch_predictions)  # [num_samples, batch_size, num_classes]
-            batch_mean = batch_predictions.mean(dim=0)  # [batch_size, num_classes]
-            batch_variance = batch_predictions.var(dim=0)
+            batch_predictions = torch.stack(batch_predictions)
+            all_predictions.append(batch_predictions)
 
-            if classification:
-                per_sample_entropy = -torch.sum(
-                    (batch_predictions + 1e-9) * torch.log(batch_predictions + 1e-9),
-                    dim=2,
-                )  # Shape: [num_samples, batch_size]
-                expected_entropy.append(
-                    per_sample_entropy.mean(dim=0)
-                )  # Shape: [batch_size]
+    all_predictions = torch.stack(
+        all_predictions, dim=1
+    )  # [num_samples, num_examples, num_classes]
 
-            mean_predictions.append(batch_mean)
-            variances.append(batch_variance)
-
-    mean_predictions = torch.cat(mean_predictions, dim=0)   # [num_examples, num_classes]
-    variances = torch.cat(variances, dim=0)
+    mean_predictions = all_predictions.mean(dim=0)
+    variances = all_predictions.var(dim=0)
 
     if classification:
-        expected_entropy = torch.cat(expected_entropy, dim=0)  # [num_examples]
         predictive_entropy = -torch.sum(
             (mean_predictions + 1e-9) * torch.log(mean_predictions + 1e-9), dim=1
         )
+
+        per_sample_entropies = -torch.sum(
+            (all_predictions + 1e-9) * torch.log(all_predictions + 1e-9), dim=2
+        )  # [num_samples, num_examples]
+        expected_entropy = per_sample_entropies.mean(dim=0)
         mutual_information = predictive_entropy - expected_entropy
+
         mean_std_deviation = torch.mean(torch.sqrt(variances), dim=1)
 
-        return (
-            mean_predictions,
-            variances,
-            predictive_entropy,
-            mutual_information,
-            mean_std_deviation,
-        )
+        max_probability, predicted_class = mean_predictions.max(dim=1)
+        variation_ratio = 1 - max_probability
 
-    return mean_predictions, variances
+        df = pd.DataFrame(
+            {
+                "Predicted_Class": predicted_class.cpu().numpy(),
+                "Predictive_Entropy": predictive_entropy.cpu().numpy(),
+                "Mutual_Information": mutual_information.cpu().numpy(),
+                "Variation_Ratio": variation_ratio.cpu().numpy(),
+                "Mean_Std_Deviation": mean_std_deviation.cpu().numpy(),
+            }
+        )
+        return df
+
+    return mean_predictions.cpu().numpy().squeeze(), variances.cpu().numpy().squeeze()
